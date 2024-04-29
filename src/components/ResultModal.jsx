d82b6e76e1a2854a02d09f50dd315c13ef5344da
gagandeep sandhu <gagandeepsandhu>
2024-04-28 23:22:33 -0400

    finished project

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -3,27 +3,42 @@ import { forwardRef, useImperativeHandle, useRef } from 'react'
 //know if the timer ran out.
 //allow the user to stop the timer.
 //know if the user stopped the timer on time.
-const ResultModal = forwardRef(({ result, target_time }, ref) => {
-    const dialog_ref = useRef()
+const ResultModal = forwardRef(
+    ({ target_time, on_close, time_remaining }, ref) => {
+        //react values
+        const dialog_ref = useRef()
 
-    useImperativeHandle(ref, () => {
-        return {
-            open: () => dialog_ref?.current?.showModal(),
-        }
-    })
-    return (
-        <dialog className='result-modal' ref={dialog_ref}>
-            <h2>You {result}</h2>
-            <p>The target time was {target_time} seconds</p>
-            <p>
-                You stopped the timer with
-                <strong>X seconds left.</strong>
-            </p>
-            <form method='dialog'>
-                <button>Close</button>
-            </form>
-        </dialog>
-    )
-})
+        useImperativeHandle(ref, () => {
+            return {
+                open: () => dialog_ref?.current?.showModal(),
+            }
+        })
+
+        //renderiing logic
+        const time_in_seconds = (time_remaining / 1000).toFixed(2)
+        const lost = time_remaining === 0
+        const score = Math.round(
+            (1 - time_remaining / (target_time * 1000)) * 100
+        )
+        return (
+            <dialog
+                className='result-modal'
+                ref={dialog_ref}
+                onClose={on_close}
+            >
+                <h2>You {lost ? 'Lost' : 'Won'}</h2>
+                <p>The target time was {target_time} seconds</p>
+                <p>Score: {score}</p>
+                <p>
+                    You stopped the timer with
+                    <strong>{time_in_seconds} seconds left.</strong>
+                </p>
+                <form method='dialog'>
+                    <button>Close</button>
+                </form>
+            </dialog>
+        )
+    }
+)
 
 export default ResultModal
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -2,12 +2,14 @@ import { useRef, useState } from 'react'
 import ResultModal from './ResultModal'
 
 const TimerChallenge = ({ title, target_time }) => {
+    // react values
     const [time_remaining, update_time_remaining] = useState(
         target_time * 1000
     )
     const dialog_ref = useRef()
     const timer = useRef()
 
+    //methods
     const start_time = () => {
         timer.current = setInterval(() => {
             update_time_remaining((prev_time) => prev_time - 10)
@@ -16,26 +18,35 @@ const TimerChallenge = ({ title, target_time }) => {
 
     const stop_time = () => {
         clearInterval(timer.current)
+        dialog_ref.current.open()
     }
 
+    const on_modal_close = () => {
+        update_time_remaining(target_time_in_milli)
+        alert('closed')
+    }
+
+    //rendering logic
+    const target_time_in_milli = target_time * 1000
     const timer_expired = time_remaining <= 0
-    const timer_started =
-        time_remaining > 0 && time_remaining < target_time * 1000
-    if (time_remaining <= 0) {
+    const timer_is_active =
+        time_remaining < target_time_in_milli && time_remaining > 0
+
+    if (timer_expired) {
         clearInterval(timer.current)
-        dialog_ref.current.open()
-    } else if (!timer && time_remaining < target_time * 1000) {
         dialog_ref.current.open()
     }
 
     const button_on_click =
-        time_remaining < target_time * 1000 ? stop_time : start_time
+        time_remaining < target_time_in_milli ? stop_time : start_time
     return (
         <>
             <ResultModal
                 result={'Lost'}
                 target_time={target_time}
+                time_remaining={time_remaining}
                 ref={dialog_ref}
+                on_close={on_modal_close}
             />
             <section className='challenge'>
                 <h2>{title}</h2>
@@ -46,13 +57,13 @@ const TimerChallenge = ({ title, target_time }) => {
                 </p>
                 <p>
                     <button onClick={button_on_click}>
-                        {timer_started
+                        {timer_is_active
                             ? 'Stop Challenge'
                             : 'Start Challenge'}
                     </button>
                 </p>
                 <p>
-                    {timer_started
+                    {timer_is_active
                         ? 'Timer is running...'
                         : 'Timer inactive'}
                 </p>
